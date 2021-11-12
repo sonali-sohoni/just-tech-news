a8a56d0815bfde0b2b404619e01c8bfb6d473de4
sonali_sohoni <ssohoni@camsbycbs.com>
2021-11-12 09:47:08 -0500

    Post routes complete

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./user-routes");
+const postRoutes = require("./post-routes");
 router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
 router.use((req, res) => {
 	res.status(404).end();
 });
