7813bd07990e9b01ab87a414c92624c6c13fdce6
sonali_sohoni <ssohoni@camsbycbs.com>
2021-11-18 11:06:51 -0500

    session login

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
+//express
 const express = require("express");
-const routes = require("./controllers");
+//sequelize
 const sequelize = require("./config/connection");
+//handlebars
 const path = require("path");
 const exhbhs = require("express-handlebars");
 const hbs = exhbhs.create({});
+//local modules
+const routes = require("./controllers");
 
+//Express initialization
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//session setting
+const session = require("express-session");
+const sequelizeStore = require("connect-session-sequelize")(session.Store);
+const sess = {
+	secret: "Super secret secret",
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new sequelizeStore({
+		db: sequelize, //connection to store session into db. creates session table.
+	}),
+};
+
+app.use(session(sess));
+
+//handlebars
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
-
+//middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
